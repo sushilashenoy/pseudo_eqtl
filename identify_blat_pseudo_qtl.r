4b4f686a2d53315e005f3a355fb97c32300ae287
Sushila Shenoy <sushila.alyssa.shenoy@gmail.com>
2016-04-04 13:11:17 -0400

    Fixed an error and added pseudo eQTL lookup from blat tables.

--- identify_blat_pseudo_qtl.r
+++ identify_blat_pseudo_qtl.r
@@ -1,5 +1,6 @@
 
-load('related_genes.RData')
+load('test_blat_matches.RData')
+colnames(blat.matches)[2] <- 'chromosome'
 load('entrez2genome.RData')
 
 # Define functions
@@ -37,70 +38,77 @@ find.distance.idx <- function(idx, chrom, min, max=min, verbose=FALSE) {
   return ( distance/1e6 )
 }
 
-# Convenience function for when we haven't already found the gene in the table
-find.distance <- function(gene, ...) {
-  idx <- match(gene, entrez2genome$entrez)
-  find.distance.idx(idx, ...)
+
+# Calculate distance (in Mb) to alignments as identified by row indices in blat.matches table
+find.match.distance.idx <- function(idx, chrom, min, max=min, verbose=FALSE) {
+  # All input vectors should be the same length, or length 1
+  stopifnot(length(chrom)==1 || length(idx)==length(chrom),
+            length(min)==1 || length(idx)==length(min),
+            length(max)==1 || length(idx)==length(max))
+  
+  same.chrom <- blat.matches$chromosome[idx] == chrom
+  dist <- with(blat.matches[idx, ], cbind(min-start, max-start, min-end, max-end))
+  
+  if ( verbose ) 
+    cat(dim(dist), '\n')
+  
+  direction <- sign(dist[, 1])
+  overlaps <- apply(dist, 1, function (d) any(sign(d[1]) != sign(d[2:4])))
+  same.chrom <- blat.matches$chromosome[idx] == chrom
+  
+  min.abs.dist <- apply(abs(dist), 1, min)
+  
+  distance <- min.abs.dist * direction * (!overlaps) 
+  distance[!same.chrom] <- NA
+  
+  
+  if ( verbose ) cat('Alignment-genotype distances:', dist, '\tOverlap:', overlaps,
+                     '\tDirection:', direction, '\tOrientation:', orient,
+                     '\tChrom match:', same.chrom, '\n')
+  
+  return ( distance/1e6 )
 }
 
 
-#' Identify trans eQTL that are cis to a related gene
-#' Returns the entrez ID of the cis related gene, or a negative number if none is found.
-#' -1 means there were no related genes
-#' -2 means there were related genes but they had no genomic coordinates
-#' -3 means none of the related genes were cis to the eqtl gene
-
-#' Side Note: For some reason there are a number of genes in the related genes
-#' table that don't have any genomic coordinates. However, we can't just delete
-#' them because there may be secondary linkages between genes that do have 
-#' coordinates.
-find.pseudo.cis <- function(gene, chrom, min, max=min, cis.threshold=1, report=FALSE) {  
+#' Identify trans eQTL that are cis to a region that aligns to the eQTL gene
+#' Returns the index of the match, or a negative number if none is found.
+#' -1 means the gene was not in the table
+#' -2 means none of the alignments were cis to the eqtl gene
+#' 
+find.blat.pseudo.cis <- function(gene, chrom, min, max=min, cis.threshold=1, report=FALSE) {  
   
   # All input vectors should be the same length!
   stopifnot(length(gene)==length(chrom), length(gene)==length(min), length(gene)==length(max))
  
   # If we have multiple loci, deal with one at a time
   if ( length(gene) > 1 )
-    return ( sapply(1:length(gene), function (i) find.pseudo.cis(gene[i], chrom[i],
+    return ( sapply(1:length(gene), function (i) find.blat.pseudo.cis(gene[i], chrom[i],
                                                                 min[i], max[i],
                                                                 cis.threshold=cis.threshold,
                                                                 report=report)) )
+
+  m.idx <- which(blat.matches$entrez==gene)
   
-  idx <- match(gene, entrez2genome$entrez)
-  partners <- with(related.genes, partner[id==gene])
-  
-  # If there are no related genes, abort
-  if ( length(partners) < 1 )
-    return ( -2 )
-  
-  # Recursively collect partners of partners (excluding geneitself)
-  plen <- 0
-  while ( plen < (plen <- length(partners)) ) 
-    partners <- setdiff(union(partners, with(related.genes, partner[id %in% partners])), gene)
-  
-  # Find partner genes in table
-  p.idx <- match(partners, entrez2genome$entrez)
-  p.idx <- p.idx[!is.na(p.idx)]
-  
-  # If no related genes have coordinates, abort
-  if ( length(p.idx) < 1 )
+  # If there are no matches, abort
+  if ( length(m.idx) < 1 )
     return ( -1 )
   
+  
   # Find distances to all partner genes
-  p.dist <- find.distance.idx(p.idx, chrom, min, max)
+  p.dist <- find.match.distance.idx(m.idx, chrom, min, max)
   
   if ( any(!is.na(p.dist) & abs(p.dist) < cis.threshold) ) {
     # Select the closest partner gene
-    best.idx <- p.idx[which.min(abs(p.dist))]
+    best.idx <- m.idx[which.min(abs(p.dist))]
     if ( report ) {
-      cat('Found cis gene', entrez2genome$symbol[best.idx], 'for',
-          entrez2genome$symbol[idx], 'trans hit (loci dist.', min(abs(p.dist)[!is.na(p.dist)]) , 'Mb).\n')
+      cat('Found cis alignment for',
+          entrez2genome$symbol[match(gene, entrez2genome$entrez)], 'trans hit (loci dist.', min(abs(p.dist)[!is.na(p.dist)]) , 'Mb).\n')
     }
-    return ( entrez2genome$entrez[best.idx] )
+    return ( best.idx )
   } 
   
   # If we get to this point, none of the related genes passed the cis threshold
-  return ( -3 )
+  return ( -2 )
 }
 
 
@@ -112,6 +120,7 @@ load('test_eqtl_data.RData')
 
 
 e2g.idx <- match(eqtl.info$gene, entrez2genome$entrez)
+
 eqtl.info$symbol <- entrez2genome$symbol[e2g.idx]
 
 # Calculate distance for all eqtl loci
@@ -123,13 +132,19 @@ eqtl.info$cis <- with(eqtl.info, !is.na(distance) & abs(distance) <= 1)
 # Identify trans eQTL and look for pseudo cis genes
 trans.idx <- which(!eqtl.info$cis)
 pseudo.results <-with(eqtl.info[trans.idx, ],
-                      find.pseudo.cis(gene, chrom, min.pos, max.pos, report=TRUE))
+                      find.blat.pseudo.cis(gene, chrom, min.pos, max.pos, report=TRUE))
 
 # If pseudo.results is a postive number, we have a pseudo cis hit
 eqtl.info$pseudo.cis <- FALSE
 eqtl.info$pseudo.cis[trans.idx] <- pseudo.results > 0
 
 # Select pseudo cis hits and calculate distances from loci to cis genes
-pseudo.cis.eqtl <- subset(eqtl.info, pseudo.cis)
-pseudo.cis.eqtl$cis.gene <- pseudo.results[pseudo.results > 0]
-pseudo.cis.eqtl$pseudo.dist <- with(pseudo.cis.eqtl, find.distance(cis.gene, chrom, min.pos, max.pos))
+pseudo.blat.eqtl <- subset(eqtl.info, pseudo.cis)
+pseudo.blat.eqtl$m.idx <- pseudo.results[pseudo.results > 0]
+pseudo.blat.eqtl$pseudo.dist <- with(pseudo.blat.eqtl, find.match.distance.idx(m.idx, chrom, min.pos, max.pos))
+
+
+pseudo.blat.eqtl$pct.score <- blat.matches$pct.score[pseudo.blat.eqtl$m.idx]
+pseudo.blat.eqtl$pct.query <- blat.matches$pct.query[pseudo.blat.eqtl$m.idx]
+pseudo.blat.eqtl$pct.id <- blat.matches$pct.id[pseudo.blat.eqtl$m.idx]
+
